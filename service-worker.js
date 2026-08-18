c7ca39fa624db857d71ef28ec102829ca303c57c
talaveravideoassist <talavera.videoassist@gmail.com>
2026-08-18 00:53:24 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'talavera-va-upgrade-v14';
+const CACHE_NAME = 'talavera-va-upgrade-v16';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
